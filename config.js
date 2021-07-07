5a2edbd98075471ad1b252d0e25a6ce96e64f76f
Sayak Maji <45718114+GeekyGeek3371@users.noreply.github.com>
2021-07-08 00:45:08 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+//store some configuration info about our server. This is way of centralizing configuration for our server
+
+module.exports = {
+    'secretKey': '12345-67890-09876-54321',
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+}
